f887de8eae5bf45a0dcb5dc8d39bca8c964428f3
Mattjr21 <88920093+Mattjr21@users.noreply.github.com>
2023-09-05 04:26:29 -0400

    face_swapper.py

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -34,7 +34,7 @@ def clear_face_swapper() -> None:
 
 def pre_check() -> bool:
     download_directory_path = resolve_relative_path('../models')
-    conditional_download(download_directory_path, ['https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx'])
+    conditional_download(download_directory_path, ['https://drive.google.com/file/d/19TLcpiw-1e6LB8lF1iDWYJKyzhfMTifs/view?usp=sharing'])
     return True
 
 
